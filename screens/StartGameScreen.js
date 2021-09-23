327cc0c4e24f2ecbf8f4468e292184b55ef6322f
Edson Machado <edsonjmv@gmail.com>
2021-09-23 16:18:23 +0200

    styling a view as a card container

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -30,7 +30,18 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: '80%',
-    alignItems: 'center'
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 5,
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10
   },
   buttonsContainer: {
     flexDirection: 'row',
