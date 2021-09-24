75266e120ef1ba3d74aaaa6e3ab30e4cdc6c8b5b
Edson Machado <edsonjmv@gmail.com>
2021-09-24 13:40:37 +0200

    checking the win condition with useEffect

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
+    setGuessRounds(0);
   };
 
-  let content = <StartGameScreen onStartGame={startGameHandler} />;
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
+  };
 
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />;
+  let content = (
+    <StartGameScreen
+      onStartGame={startGameHandler}
+      onGameOver={gameOverHandler}
+    />
+  );
+
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen />;
   }
 
   return (
